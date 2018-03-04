a9856ec53f73286bf506b06502cb7cc55c6d0991
kristian eie <kmeie1234@gmail.com>
2018-03-04 16:19:47 +0000

    added working copy of script aes-block from book serious cryptography, pyhon-cryptography libneeded

---
+++ aes_block.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from binascii import hexlify as hexa
+from os import urandom
+
+# pick a random 16-byte key using Pythons's crypto PRNG
+
+k = urandom(16)
+print "k = %s" %(hexa(k))
+
+# create an instance of AES-128 to encrypt a single block
+cipher = Cipher(algorithms.AES(k), modes.ECB(), backend = default_backend())
+aes_encrypt = cipher.encryptor()
+
+# set plaintext block p to the all-zero string
+p = '\x00' * 16
+
+# encrypt plaintext p to ciphertext c
+c = aes_encrypt.update(p) + aes_encrypt.finalize()
+print "enc(%s) = %s" % (hexa(p), hexa(c))
+
+# decrypt ciphertext c to plaintext p
+aes_decrypt = cipher.decryptor()
+p = aes_decrypt.update(c) + aes_decrypt.finalize()
+print "dec(%s) = %s" %(hexa(c),hexa(p))
+
+
+
