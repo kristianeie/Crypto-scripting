ef538985487250265aed8d447303eab0c8cdc1be
kristian eie <kmeie1234@gmail.com>
2018-03-05 15:05:46 +0000

    added working copy of aes_cbc.py mode of operation

---
+++ aes_cbc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from binascii import hexlify as hexa
+from os import urandom
+
+BLOCKLEN = 16
+# the blocks() function splits data string into space-speparated blocks
+
+def blocks(data):
+	split = [hexa(data[i:i+BLOCKLEN]) for i in range(0, len(data), BLOCKLEN)]
+	return ' '.join(split)
+k = urandom(16)
+print "k = %s" % hexa(k)
+
+# pick a random Initialization Vector (IV)
+
+iv = urandom(16)
+print "iv = %s" % hexa(iv)
+
+# pick an instance of AES in CBC-mode
+aes = Cipher(algorithms.AES(k), modes.CBC(iv), backend=default_backend()).encryptor()
+
+p = '\x00' * BLOCKLEN * 2
+c = aes.update(p) + aes.finalize()
+
+print "enc(%s) = %s" % (blocks(p), blocks(c))
+
+
+
